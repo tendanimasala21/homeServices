3ceb4b1eced860f6eabea200242fc8a562c68ad8
Tendani Masala <tendanimasala@gmail.com>
2025-11-26 20:20:53 +0200

    changes to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "./context/CartContext";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
 
 export const metadata: Metadata = {
   title: "RUTE | On-demand Services",
@@ -26,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white text-gray-900">
-        <CartProvider> {children} </CartProvider>
+        <CartProvider>{children}</CartProvider>
       </body>
     </html>
   );
